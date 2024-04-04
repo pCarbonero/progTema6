a93f8328e5644a4b11aa68739f91b609b3179ba3
pCarbonero <pablo.carbonero@iesnervion.es>
2024-04-04 20:03:09 +0200

    [upd] array

---
+++ Tema6/src/primerosherencia/ejercicio04/Ejecutable.java
@@ -0,0 +1,21 @@
+package primerosherencia.ejercicio04;
+
+public class Ejecutable {
+	public static void main(String[] args) {
+		Electrodomestico[] listaE = new Electrodomestico[10];
+		
+		Lavadora lv = new Lavadora(100, "BLANCO", 'B', 20, 10);
+		Television tv = new Television(2000, "NEGRO", 'E', 8, 20, false);
+		
+		listaE[0] = lv;
+		listaE[1] = tv;
+		
+	    for (Electrodomestico item : listaE) {
+	        if (item != null) {
+	            item.precioFinal();
+	            System.out.println("Precio final del electrodomestico: " + item.getPrecioBase());
+	        }
+	    }
+	}
+
+}
