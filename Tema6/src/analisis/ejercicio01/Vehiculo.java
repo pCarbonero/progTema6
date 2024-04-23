6f3825627ca38f33c7dc00e89469b93615dc3934
pCarbonero <pablo.carbonero@iesnervion.es>
2024-04-23 14:38:57 +0200

    qq

---
+++ Tema6/src/analisis/ejercicio01/Vehiculo.java
@@ -0,0 +1,34 @@
+package analisis.ejercicio01;
+/**
+ * 
+ */
+public class Vehiculo {
+	/**
+	 * Atributo para la marca del vehiculo
+	 */
+	private String marca;
+	/**
+	 * Atributo para el modelo del vehiculo
+	 */
+	private String modelo;
+	/**
+	 * Atributo para el color del vehiculo
+	 */
+	private String color;
+	/**
+	 * Atributo para la matricula del vehiculo
+	 */
+	private String matricula;
+	/**
+	 * Atributo que indica el estado del motor
+	 */
+	protected boolean motor = false;
+	/**
+	 * Atributo que indica la velocidad actual 
+	 */
+	protected double velocidad = 0;
+	/**
+	 * Atributo que indica la marcha actual
+	 */
+	protected int marcha = 0;
+}// clase vehiculo
