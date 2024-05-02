3e274e5156e102cbd8fecfdb33c76e89e79a9edb
pCarbonero <pablo.carbonero@iesnervion.es>
2024-05-02 21:19:53 +0200

    [upd] ej 03 interfaces comparTo

--- Tema6/src/interfaces/ejercicio03/AnimalDomestico.java
+++ Tema6/src/interfaces/ejercicio03/AnimalDomestico.java
@@ -1,34 +1,34 @@
 package interfaces.ejercicio03;
 
-public abstract class AnimalDomestico implements Animal{
+public abstract class AnimalDomestico implements Animal, Comparable<AnimalDomestico>{
 	
 	private String nombre;
 	private String raza;
-	private String peso;
+	private int peso;
 	private String color;
 	
 	
 	public void vacunar() {
-		
+		System.out.println("Vacunado :D");
 	}
 	
-	public boolean hacerCaso() {
-		return false;	
-	}
+	public abstract boolean hacerCaso();
 	
 	@Override
 	public void comer() {
-		
+		this.peso+=1;
 	}
 
 	@Override
 	public void dormir() {
-		
+		System.out.println("zZZZz");
 	}
 
 	@Override
-	public void hacerRuido() {
-		
+	public abstract void hacerRuido();
+	
+	@Override
+	public int compareTo(AnimalDomestico a) {
+		return this.peso - a.peso;
 	}
-
 }
